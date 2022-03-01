bd6a6c40fdb608adb89e9d1dc7cac14c626dee03
Christian Timbe <bobitlmr188221@spu.ac.ke>
2022-03-01 08:15:08 -0800

    Implemented search button functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 // TODO: Add responsiveness from the menu bar
-// TODO: Implement search button 
+let searchBox = document.querySelector(".search-form")
+let searchButton = document.querySelector(".search-button")
+let closeButton = document.querySelector("#close")
+searchButton.addEventListener("click", function(){
+    searchBox.style.top = 0
+})
+closeButton.addEventListener("click", function(){
+    searchBox.style.top = "-110%"
+})
 // TODO: Implement  swiper on homepage
