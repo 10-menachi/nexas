7520d96e6dac567b97d0ac993dc8875f007f5b2c
Christian Timbe <bobitlmr188221@spu.ac.ke>
2022-03-01 10:51:39 -0800

    implemented the swipeable functionality half-way

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ closeButton.addEventListener("click", function(){
     searchBox.style.top = "-110%"
 })
 // TODO: Implement  swiper on homepage
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
